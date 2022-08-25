a71039f9512e557b7a46cfef7056cbabceeacd12
Sharonah8 <nyamongosharon88@gmail.com>
2022-08-25 21:22:11 +0300

    Task lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener("submit", () => {
+    e.preventDefault()
+    buildTaskLister(e.target.new - task - RTCSessionDescription.value)
+  });
 });
+
+
+
+function taskLister(todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo}`
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#create-task-form').appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
